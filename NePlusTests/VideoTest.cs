9366bba04f6c07ea51f3344151086b724aa199c9
Christopher Horenstein <chris.horenstein@gmail.com>
2010-10-30 10:41:15 -0700

    Added testing project to source code, and stubbed a video testing class.

---
+++ NePlusTests/VideoTest.cs
@@ -0,0 +1,21 @@
+﻿using NePlus;
+using NePlus.EngineComponents;
+
+using NUnit.Framework;
+
+namespace NePlusTests
+{
+    [TestFixture]
+    public class VideoTest
+    {
+        [Test]
+        public void TestVideo()
+        {
+            Game game = new Game();
+            Engine engine = new Engine(game);
+
+            Assert.AreEqual(engine.Video.Height, 720);
+            Assert.AreEqual(engine.Video.Width, 1280);
+        }
+    }
+}
