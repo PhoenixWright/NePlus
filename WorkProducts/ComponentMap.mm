59e47361d04b7678cede811f41c6e1cc58881975
Christopher Horenstein <chris.horenstein@gmail.com>
2010-11-25 02:26:53 -0800

    Edited map file to get rid of a web reference that Visual Studio apparently couldn't resolve unless connected to the net. Also updated the ComponentMap with some important light ideas (thanks, Woody, for mentioning them).

--- WorkProducts/ComponentMap.mm
+++ WorkProducts/ComponentMap.mm
@@ -1,6 +1,6 @@
 <map version="0.8.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node COLOR="#ff0000" CREATED="1286305883776" ID="Freemind_Link_313934408" MODIFIED="1286325503700" STYLE="bubble" TEXT="Ne+">
+<node COLOR="#000000" CREATED="1286305883776" ID="Freemind_Link_313934408" MODIFIED="1290629796297" STYLE="bubble" TEXT="Ne+">
 <font NAME="Courier New" SIZE="12"/>
 <node CREATED="1286306132499" ID="Freemind_Link_594671218" MODIFIED="1286325503699" POSITION="right" STYLE="bubble" TEXT="Gameplay">
 <font NAME="Courier New" SIZE="12"/>
@@ -8,52 +8,9 @@
 <font NAME="Courier New" SIZE="12"/>
 <node CREATED="1286324017770" ID="Freemind_Link_586962709" MODIFIED="1286325492022" TEXT="AIComponent&#xa;-Goals&#xa;-Mood&#xa;-Needs">
 <font NAME="Courier New" SIZE="12"/>
-<node CREATED="1286332296536" ID="Freemind_Link_66991977" MODIFIED="1286332310355" TEXT="EnemyComponent"/>
-</node>
-</node>
-<node CREATED="1286306228777" ID="Freemind_Link_1407156829" MODIFIED="1286325503698" TEXT="Objects">
-<font NAME="Courier New" SIZE="12"/>
-<node CREATED="1286332093756" ID="Freemind_Link_50162927" MODIFIED="1286332127126" TEXT="Characters">
-<font NAME="Courier New" SIZE="12"/>
-<node CREATED="1286332107019" ID="Freemind_Link_1199089376" MODIFIED="1286332127127" TEXT="PlayerComponent">
+<node CREATED="1286332296536" ID="Freemind_Link_66991977" MODIFIED="1290629400465" TEXT="EnemyComponent">
 <font NAME="Courier New" SIZE="12"/>
 </node>
-</node>
-<node COLOR="#ff0000" CREATED="1286308208294" ID="Freemind_Link_1375182525" LINK="http://gamedev.stackexchange.com/questions/4114/how-can-i-design-an-effective-game-object-interaction-scheme-with-a-component-bas" MODIFIED="1286325503693" TEXT="Light&#xa;-Brightness&#xa;-Glow&#xa;-ProjectionAngle&#xa;-Pulse">
-<font NAME="Courier New" SIZE="12"/>
-<node COLOR="#ff0000" CREATED="1286323360780" ID="Freemind_Link_1166265120" MODIFIED="1286325503693" TEXT="AmplifierLightComponent">
-<font NAME="Courier New" SIZE="12"/>
-</node>
-<node COLOR="#ff0000" CREATED="1286309763068" ID="Freemind_Link_1550570505" MODIFIED="1286325503692" TEXT="FrictionLightComponent">
-<font NAME="Courier New" SIZE="12"/>
-</node>
-<node COLOR="#ff0000" CREATED="1286309748177" ID="Freemind_Link_444232285" MODIFIED="1286325503692" TEXT="GravityLightComponent">
-<font NAME="Courier New" SIZE="12"/>
-</node>
-<node COLOR="#ff0000" CREATED="1286309815175" ID="Freemind_Link_643624227" MODIFIED="1286325503691" TEXT="NullLightComponent">
-<font NAME="Courier New" SIZE="12"/>
-</node>
-<node COLOR="#ff0000" CREATED="1286309828726" ID="Freemind_Link_1370133641" MODIFIED="1286325503691" TEXT="PlatformLightComponent">
-<font NAME="Courier New" SIZE="12"/>
-</node>
-<node COLOR="#ff0000" CREATED="1286323327663" ID="Freemind_Link_507319248" MODIFIED="1286325503690" TEXT="RefractorLightComponent">
-<font NAME="Courier New" SIZE="12"/>
-</node>
-<node COLOR="#ff0000" CREATED="1286309778682" ID="Freemind_Link_1603961661" MODIFIED="1286325503689" TEXT="SpeedLightComponent">
-<font NAME="Courier New" SIZE="12"/>
-</node>
-<node COLOR="#ff0000" CREATED="1286323347917" ID="Freemind_Link_825269705" MODIFIED="1286325503688" TEXT="WarpLightComponent">
-<font NAME="Courier New" SIZE="12"/>
-</node>
-</node>
-</node>
-<node CREATED="1286306217994" ID="Freemind_Link_278367095" MODIFIED="1286325503688" TEXT="Scenes">
-<font NAME="Courier New" SIZE="12"/>
-<node CREATED="1286323784050" ID="Freemind_Link_1075871436" MODIFIED="1286325503688" TEXT="Introduction">
-<font NAME="Courier New" SIZE="12"/>
-</node>
-<node CREATED="1286323794465" ID="Freemind_Link_880900932" MODIFIED="1286325503687" TEXT="Level1">
-<font NAME="Courier New" SIZE="12"/>
 </node>
 </node>
 </node>
@@ -63,6 +20,7 @@
 <font NAME="Courier New" SIZE="12"/>
 <node CREATED="1286324950574" ID="Freemind_Link_819408485" MODIFIED="1286325503682" TEXT="Characters&#xa;-Animations">
 <font NAME="Courier New" SIZE="12"/>
+<node CREATED="1290630170071" ID="Freemind_Link_180003439" MODIFIED="1290630172382" TEXT="Player"/>
 </node>
 <node CREATED="1286324984859" ID="Freemind_Link_233021102" MODIFIED="1286325503682" TEXT="Scenes">
 <font NAME="Courier New" SIZE="12"/>
@@ -76,11 +34,14 @@
 </node>
 <node CREATED="1286324279664" ID="Freemind_Link_476613908" MODIFIED="1286325503680" TEXT="Effects">
 <font NAME="Courier New" SIZE="12"/>
-<node COLOR="#000000" CREATED="1286324281681" ID="Freemind_Link_1062299512" MODIFIED="1286332348531" TEXT="Particle Effects">
+<node COLOR="#000000" CREATED="1286324281681" ID="Freemind_Link_1062299512" MODIFIED="1290633481905" TEXT="Particle Effects">
 <font NAME="Courier New" SIZE="12"/>
-<node COLOR="#000000" CREATED="1286324293231" ID="Freemind_Link_241790846" MODIFIED="1286332348532" TEXT="Electricity">
+<icon BUILTIN="button_ok"/>
+<node COLOR="#000000" CREATED="1286324293231" ID="Freemind_Link_241790846" MODIFIED="1290629593239" TEXT="Electricity">
 <font NAME="Courier New" SIZE="12"/>
-<icon BUILTIN="help"/>
+</node>
+<node CREATED="1290631265372" ID="Freemind_Link_666816906" MODIFIED="1290631277230" TEXT="Gravity">
+<icon BUILTIN="pencil"/>
 </node>
 </node>
 <node CREATED="1286324287184" ID="Freemind_Link_1447479798" MODIFIED="1286325503678" TEXT="Post-Processing Effects">
@@ -91,13 +52,14 @@
 </node>
 <node CREATED="1286325136156" ID="Freemind_Link_1387304719" MODIFIED="1286325503677" TEXT="Shaders">
 <font NAME="Courier New" SIZE="12"/>
-<node CREATED="1286324344538" ID="Freemind_Link_1197852159" LINK="http://www.catalinzima.com/2010/07/my-technique-for-the-shader-based-dynamic-2d-shadows/" MODIFIED="1286325503676" TEXT="Lighting">
+<node CREATED="1290629522416" ID="Freemind_Link_788351647" MODIFIED="1290629525163" TEXT="Lighting"/>
+</node>
+</node>
+<node CREATED="1286332200469" ID="Freemind_Link_631280434" MODIFIED="1290629339381" TEXT="Text">
+<font NAME="Courier New" SIZE="12"/>
+<node CREATED="1286332203666" ID="Freemind_Link_1514580022" MODIFIED="1290629368272" TEXT="TextComponent">
 <font NAME="Courier New" SIZE="12"/>
 </node>
-</node>
-</node>
-<node CREATED="1286332200469" ID="Freemind_Link_631280434" MODIFIED="1286332202239" TEXT="Text">
-<node CREATED="1286332203666" ID="Freemind_Link_1514580022" MODIFIED="1286332207309" TEXT="TextComponent"/>
 </node>
 </node>
 <node CREATED="1286306109317" ID="Freemind_Link_270443517" MODIFIED="1286325503676" POSITION="left" STYLE="bubble" TEXT="Audio" VSHIFT="1">
@@ -106,7 +68,7 @@
 <font NAME="Courier New" SIZE="12"/>
 <node CREATED="1286325033894" ID="Freemind_Link_616147364" MODIFIED="1286325503674" TEXT="Player">
 <font NAME="Courier New" SIZE="12"/>
-<node CREATED="1286325346087" ID="Freemind_Link_1063449715" MODIFIED="1286325503674" TEXT="Actions">
+<node CREATED="1286325346087" ID="Freemind_Link_1063449715" MODIFIED="1290629415573" TEXT="Actions">
 <font NAME="Courier New" SIZE="12"/>
 <node CREATED="1286325370789" ID="Freemind_Link_762801764" MODIFIED="1286325503673" TEXT="Attack">
 <font NAME="Courier New" SIZE="12"/>
@@ -118,9 +80,9 @@
 <font NAME="Courier New" SIZE="12"/>
 </node>
 </node>
-<node CREATED="1286325040230" ID="Freemind_Link_948221829" MODIFIED="1286325503666" TEXT="Footsteps">
-<font NAME="Courier New" SIZE="11"/>
-<node CREATED="1286325059060" ID="Freemind_Link_552672833" MODIFIED="1286325503665" TEXT="Ground">
+<node CREATED="1286325040230" ID="Freemind_Link_948221829" MODIFIED="1290629831124" TEXT="Footsteps">
+<font NAME="Courier New" SIZE="12"/>
+<node CREATED="1286325059060" ID="Freemind_Link_552672833" MODIFIED="1290632375522" TEXT="Standard">
 <font NAME="Courier New" SIZE="12"/>
 </node>
 <node CREATED="1286325064195" ID="Freemind_Link_1934587708" MODIFIED="1286325503664" TEXT="Light">
@@ -136,10 +98,87 @@
 </node>
 </node>
 </node>
-<node COLOR="#ff0000" CREATED="1286324902291" ID="Freemind_Link_1743528239" MODIFIED="1286325586395" POSITION="left" TEXT="Input">
+<node CREATED="1290629851877" ID="Freemind_Link_270697964" MODIFIED="1290633609241" POSITION="left" TEXT="Physics&#xa; - Farseer Physics Engine">
+<node CREATED="1290629871635" ID="Freemind_Link_1574349718" MODIFIED="1290630097065" TEXT="Engine&#xa; - Gravity&#xa; - World"/>
+<node CREATED="1290629881954" ID="Freemind_Link_453355384" MODIFIED="1290633722856" TEXT="PhysicsComponents">
+<node CREATED="1290629939729" ID="Freemind_Link_236300994" MODIFIED="1290632993508" TEXT="MovingComponent&#xa; - List&lt;Point&gt; (Specific order)&#xa; - ???StopModifier???&#xa;   - To define how gradual the stop is at each position&#xa; - Velocity"/>
+<node CREATED="1290629945524" ID="Freemind_Link_816247003" MODIFIED="1290632548299" TEXT="PendulumComponent&#xa; - Pivot Position&#xa; - Weight Position&#xa; - Horizontal Force&#xa;   - This keeps the pendulum consistent">
+<icon BUILTIN="pencil"/>
+</node>
+<node CREATED="1290630119644" ID="Freemind_Link_959315157" MODIFIED="1290630447339" TEXT="PlayerComponent">
+<icon BUILTIN="pencil"/>
+</node>
+</node>
+</node>
+<node CREATED="1286306217994" ID="Freemind_Link_278367095" MODIFIED="1286325503688" POSITION="right" TEXT="Scenes">
 <font NAME="Courier New" SIZE="12"/>
-<node COLOR="#ff0000" CREATED="1286324927425" ID="Freemind_Link_819002596" MODIFIED="1286325503659" TEXT="InputComponent">
+<node CREATED="1286323784050" ID="Freemind_Link_1075871436" MODIFIED="1286325503688" TEXT="Introduction">
 <font NAME="Courier New" SIZE="12"/>
+</node>
+<node CREATED="1286323794465" ID="Freemind_Link_880900932" MODIFIED="1286325503687" TEXT="Level1">
+<font NAME="Courier New" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1286306228777" ID="Freemind_Link_1407156829" MODIFIED="1290630069196" POSITION="right" TEXT="GameObjects">
+<font NAME="Courier New" SIZE="12"/>
+<node CREATED="1286332093756" ID="Freemind_Link_50162927" MODIFIED="1286332127126" TEXT="Characters">
+<font NAME="Courier New" SIZE="12"/>
+<node CREATED="1286332107019" ID="Freemind_Link_1199089376" MODIFIED="1290633774467" TEXT="Player">
+<font NAME="Courier New" SIZE="12"/>
+<icon BUILTIN="pencil"/>
+</node>
+</node>
+<node CREATED="1290629702712" ID="Freemind_Link_1280843989" MODIFIED="1290629771883" TEXT="Light&#xa; - Brightness&#xa; - Glow&#xa; - Pulse">
+<node COLOR="#000000" CREATED="1286309763068" ID="Freemind_Link_1550570505" MODIFIED="1290629657207" TEXT="FrictionLightComponent">
+<font NAME="Courier New" SIZE="12"/>
+</node>
+<node COLOR="#000000" CREATED="1286309748177" ID="Freemind_Link_444232285" MODIFIED="1290630477089" TEXT="GravityLightComponent">
+<font NAME="Courier New" SIZE="12"/>
+<icon BUILTIN="button_ok"/>
+</node>
+<node COLOR="#000000" CREATED="1286309828726" ID="Freemind_Link_1370133641" MODIFIED="1290630646653" TEXT="PlatformLightComponent">
+<font NAME="Courier New" SIZE="12"/>
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1290680596592" ID="Freemind_Link_1903595776" MODIFIED="1290680600279" TEXT="SizeLightComponent"/>
+<node COLOR="#000000" CREATED="1286309778682" ID="Freemind_Link_1603961661" MODIFIED="1290629657191" TEXT="SpeedLightComponent">
+<font NAME="Courier New" SIZE="12"/>
+</node>
+<node CREATED="1290680570566" ID="Freemind_Link_243252178" MODIFIED="1290680648564" TEXT="TimeLightComponent&#xa; - Things have a high velocity/force&#xa;  - But still go slowly"/>
+<node COLOR="#000000" CREATED="1286323347917" ID="Freemind_Link_825269705" MODIFIED="1290631246349" TEXT="WarpLightComponent">
+<font NAME="Courier New" SIZE="12"/>
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1290633511395" ID="Freemind_Link_1755396443" MODIFIED="1290633519244" TEXT="LightModifiers">
+<node COLOR="#000000" CREATED="1286323360780" ID="Freemind_Link_1166265120" MODIFIED="1290629657191" TEXT="AmplifierLightComponent">
+<font NAME="Courier New" SIZE="12"/>
+</node>
+<node CREATED="1290633559123" ID="Freemind_Link_383970236" MODIFIED="1290633570654" TEXT="NullLightComponent">
+<icon BUILTIN="pencil"/>
+</node>
+<node COLOR="#000000" CREATED="1286323327663" ID="Freemind_Link_507319248" MODIFIED="1290629657207" TEXT="RefractorLightComponent">
+<font NAME="Courier New" SIZE="12"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1290630678935" ID="Freemind_Link_960040068" MODIFIED="1290630682011" POSITION="left" TEXT="Development Tools">
+<node CREATED="1290630683233" ID="Freemind_Link_588310259" MODIFIED="1290630746759" TEXT="Level Editor&#xa; - Tiled"/>
+<node CREATED="1290630687547" ID="Freemind_Link_1482355391" MODIFIED="1290630802614" TEXT="Particle Effect Editor&#xa; - Mercury Particle Engine"/>
+</node>
+<node CREATED="1290630697039" ID="Freemind_Link_325231794" MODIFIED="1290631222918" POSITION="right" TEXT="Engine">
+<node CREATED="1290632255312" ID="Freemind_Link_651527312" MODIFIED="1290632320809" TEXT="Camera">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1290632336957" ID="Freemind_Link_697044004" MODIFIED="1290633038171" TEXT="Configuration">
+<icon BUILTIN="pencil"/>
+</node>
+<node COLOR="#000000" CREATED="1286324927425" ID="Freemind_Link_819002596" MODIFIED="1290630561688" TEXT="InputComponent">
+<font NAME="Courier New" SIZE="12"/>
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1290630808080" ID="Freemind_Link_758827205" MODIFIED="1290633626947" TEXT="Level&#xa; - Light List&#xa; - Load Tiled Level Files">
+<icon BUILTIN="button_ok"/>
 </node>
 </node>
 </node>
